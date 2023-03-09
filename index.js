6a1841adc1b8f65daa8f11edfda8ac05d936e47e
xXLuisHenriqueXx <127408909+xXLuisHenriqueXx@users.noreply.github.com>
2023-03-09 20:45:03 -0300

    Adicionando arquivos

---
+++ index.js
@@ -0,0 +1,9 @@
+let docTittle = document.title;
+
+window.addEventListener("blur", ()=>{
+    document.title = "Volte Aqui!";
+})
+
+window.addEventListener("focus", ()=>{
+    document.title = docTittle ;
+})
